083bfcb37219046cc9ac1f31a42b1bbce77c44a3
ronething <axingfly@gmail.com>
2020-03-29 22:14:09 +0800

    feat: router group

--- gee/gee.go
+++ gee/gee.go
@@ -6,31 +6,54 @@
 package gee
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(c *Context)
 
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup // store all groups
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	engine      *Engine       // all groups share a Engine instance
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (e *Engine) addRoute(method, pattern string, handler HandlerFunc) {
-	// TODO: 重复路由判断？
-	e.router.addRoute(method, pattern, handler)
+func (g *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := g.engine
+	newGroup := &RouterGroup{
+		prefix: g.prefix + prefix,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
-func (e *Engine) GET(pattern string, handler HandlerFunc) {
-	e.addRoute("GET", pattern, handler)
+func (g *RouterGroup) addRoute(method, comp string, handler HandlerFunc) {
+	pattern := g.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	g.engine.router.addRoute(method, pattern, handler)
 }
 
-func (e *Engine) POST(pattern string, handler HandlerFunc) {
-	e.addRoute("POST", pattern, handler)
+func (g *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	// 注意：这里是 g.addRoute 并非 g.engine.addRoute, 下面 POST 同理
+	g.addRoute("GET", pattern, handler)
+}
+
+func (g *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	g.addRoute("POST", pattern, handler)
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -39,8 +62,8 @@ func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (e *Engine) Run(addr string) (err error) {
-	for k, v := range e.router.handlers {
-		fmt.Println(k, v)
-	}
+	//for k, v := range e.router.handlers {
+	//	fmt.Println(k, v)
+	//}
 	return http.ListenAndServe(addr, e)
 }
